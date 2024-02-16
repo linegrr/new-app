481eb7616e19c59dd96c65e054082637c5801480
linegrr <line.granseth@gmail.com>
2024-02-16 16:10:59 +0100

    added search-function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTHML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = `e90345b7ffd471b6670o5atadb5d7dc5`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
